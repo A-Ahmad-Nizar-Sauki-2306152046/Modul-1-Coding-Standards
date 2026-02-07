ee18a8fb3ccb0525372f71b4db39530024a0f36b
Ahmad Nizar Sauki <anizarsauki@gmail.com>
2026-02-07 18:11:54 +0700

    add ID in create method, add findProductById and editProduct methods

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,18 +6,44 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
    private List<Product> productData = new ArrayList<>();
 
    public Product create(Product product) {
+
+      if (product.getProductId() == null) {
+         product.setProductId(UUID.randomUUID().toString());
+      }
+
       productData.add(product);
       return product;
    }
 
    public Iterator<Product> findAll() {
       return productData.iterator();
+   }
+
+   public Product findProductById(String productId) {
+      for (Product product : productData) {
+         if (product.getProductId().equals(productId)) {
+            return product;
+         }
+      }
+      return null;
+   }
+
+   public Product editProduct(Product product) {
+      for (int i = 0; i < productData.size(); i++) {
+         Product productSaved = productData.get(i);
+         if (productSaved.getProductId().equals(product.getProductId())) {
+            productData.set(i, product);
+            return product;
+         }
+      }
+      return null;
    }
 
 }
