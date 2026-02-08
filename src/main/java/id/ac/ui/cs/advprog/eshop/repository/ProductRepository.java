e2b27c2de9ad34676d2d0cdf0aa21077a0beb394
Ahmad Nizar Sauki <anizarsauki@gmail.com>
2026-02-08 11:17:12 +0700

    add delete method

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -46,4 +46,8 @@ public class ProductRepository {
       return null;
    }
 
+   public void delete(String productId) {
+      productData.removeIf(product -> product.getProductId().equals(productId));
+   }
+
 }
